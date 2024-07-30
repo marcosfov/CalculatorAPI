d408e7d4a2c552ca1edf12009f33e068b8251243
Marcos Valeriano <marcosfov@gmail.com>
2024-07-30 15:33:44 -0300

    first commit

---
+++ CalculatorAPI/Models/Calculadora.cs
@@ -0,0 +1,38 @@
+﻿namespace CalculatorAPI.Models
+{
+    public class Calculadora
+    {
+        public double Value1 { get; set; }
+        public double Value2 { get; set; }
+        public double Result { get; set; }
+
+
+
+        public Calculadora(double value1, double value2)
+        {
+            this.Value1 = value1;
+            this.Value2 = value2;
+        }
+
+        public void sum()
+        {
+            Result = Value1 + Value2;
+        }
+
+        public void subtract()
+        {
+            Result = Value1 - Value2;
+        }
+
+        public void multiply()
+        {
+            Result = Value1 * Value2;
+        }
+
+        public void divide()
+        {
+            Result = Value1 / Value2;
+        }
+
+    }
+}
